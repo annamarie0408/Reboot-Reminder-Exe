fdb5d9c31e3eac7c16084c033336ed1afc796e62
Anna Marie <annamarie0408@gmail.com>
2019-11-07 10:37:28 -0500

    Add files via upload

---
+++ mainForm.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace RebootReminder
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.rebootBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.internalTimer = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rebootBtn
+            // 
+            this.rebootBtn.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.rebootBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rebootBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rebootBtn.Location = new System.Drawing.Point(165, 273);
+            this.rebootBtn.Name = "rebootBtn";
+            this.rebootBtn.Size = new System.Drawing.Size(139, 51);
+            this.rebootBtn.TabIndex = 0;
+            this.rebootBtn.Text = "Reboot Now";
+            this.rebootBtn.UseVisualStyleBackColor = false;
+            this.rebootBtn.Click += new System.EventHandler(this.rebootBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.cancelBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelBtn.Location = new System.Drawing.Point(322, 273);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(139, 51);
+            this.cancelBtn.TabIndex = 1;
+            this.cancelBtn.Text = "Delay";
+            this.cancelBtn.UseVisualStyleBackColor = false;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label4.Location = new System.Drawing.Point(50, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(544, 37);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "You need to reboot your computer!";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Font = new System.Drawing.Font("Imprint MT Shadow", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerLabel.ForeColor = System.Drawing.Color.Black;
+            this.timerLabel.Location = new System.Drawing.Point(197, 180);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(0, 63);
+            this.timerLabel.TabIndex = 8;
+            // 
+            // internalTimer
+            // 
+            this.internalTimer.Enabled = true;
+            this.internalTimer.Interval = 1000;
+            this.internalTimer.Tick += new System.EventHandler(this.internalTimer_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 13F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(157, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(326, 21);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "This Window will automatically close";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 13F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(189, 145);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(249, 21);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "and reboot the computer in:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox1.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 10);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 373);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(640, 10);
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox3.Location = new System.Drawing.Point(630, 5);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(10, 378);
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox4.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(10, 383);
+            this.pictureBox4.TabIndex = 14;
+            this.pictureBox4.TabStop = false;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(640, 382);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.rebootBtn);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "mainForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button rebootBtn;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Timer internalTimer;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ mainForm.cs
@@ -0,0 +1,272 @@
+﻿//This is the new and improved reboot reminder created by Anna Collins and Gary Covey
+ 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Configuration;
+using System.Threading;
+using System.IO;
+using Microsoft.Win32;
+
+
+
+namespace RebootReminder
+{
+    public partial class mainForm : Form
+    {
+
+        public static string filePath = (Directory.GetCurrentDirectory() + @"\RebootReminder.exe.config");
+
+        //private static int countHour = int.Parse(ConfigurationManager.AppSettings["CountdownTimerHour"]);
+        //   private static int CountMin = int.Parse(ConfigurationManager.AppSettings["CountdownTimerMinute"]);
+        //   private static int CountSec = int.Parse(ConfigurationManager.AppSettings["CountdownTimerSeconds"]);
+
+      //  private static int RemHour = int.Parse(ConfigurationManager.AppSettings["cancelReminderTimerHour"]);
+    //    private static int RemMin = int.Parse(ConfigurationManager.AppSettings["cancelReminderTimerMinutes"]);
+      //  private static int RemSec = int.Parse(ConfigurationManager.AppSettings["cancelReminderTimerSeconds"]);
+
+        public static int countHour()
+        {
+            int countHour;
+            if (File.Exists(filePath))               
+            {
+                countHour = int.Parse(ConfigurationManager.AppSettings["CountdownTimerHour"]);
+                return countHour;
+            }
+            else
+            {
+                countHour = 1;
+            }
+            return countHour;
+        }
+        public static int CountMin()
+        {
+            int CountMin;
+            if (File.Exists(filePath))
+            {
+                CountMin = int.Parse(ConfigurationManager.AppSettings["CountdownTimerMinute"]);
+                return CountMin;
+            }
+            else
+            {
+                CountMin = 0;
+            }
+            return CountMin;
+        }
+        public static int CountSec()
+        {
+            int CountSec;
+            if (File.Exists(filePath))
+            {
+                CountSec = int.Parse(ConfigurationManager.AppSettings["CountdownTimerSeconds"]);
+                return CountSec;
+            }
+            else
+            {
+                CountSec = 0;
+            }
+            return CountSec;
+        }
+
+        public static int RemHour()
+        {
+            int RemHour;
+            if (File.Exists(filePath))
+            {
+                RemHour = int.Parse(ConfigurationManager.AppSettings["cancelReminderTimerHour"]);
+                return RemHour;
+            }
+            else
+            {
+                RemHour = 1;
+            }
+            return RemHour;
+        }
+        public static int RemMin()
+        {
+            int RemMin;
+            if (File.Exists(filePath))
+            {
+                RemMin = int.Parse(ConfigurationManager.AppSettings["cancelReminderTimerMinutes"]);
+                return RemMin;
+            }
+            else
+            {
+                RemMin = 0;
+            }
+            return RemMin;
+        }
+        public static int RemSec()
+        {
+            int RemSec;
+            if (File.Exists(filePath))
+            {
+                RemSec = int.Parse(ConfigurationManager.AppSettings["cancelReminderTimerSeconds"]);
+                return RemSec;
+            }
+            else
+            {
+                RemSec = 0;
+            }
+            return RemSec;
+        }
+                
+
+        private TimeSpan span = new TimeSpan(countHour(), CountMin(), CountSec());//private TimeSpan span = new TimeSpan(0, 1, 15);      
+        private TimeSpan reminder = new TimeSpan(RemHour(), RemMin(), RemSec());  //private TimeSpan reminder = new TimeSpan(1, 0, 0); 
+        
+        
+        public int getUpTime()
+        {
+            
+            int systemUpTimeHours;
+           
+           
+            PerformanceCounter pc = new PerformanceCounter("System", "System Up Time");
+                 pc.NextValue();
+                 systemUpTimeHours = ((int)pc.NextValue() / 3600);
+                 return systemUpTimeHours;
+             } 
+
+
+                public mainForm()
+        {
+            
+            using (StreamWriter w = File.AppendText(Directory.GetCurrentDirectory() + @"\log.txt"))
+            {
+                w.WriteLine("                  ********************************Fresh Launch********************************");
+            }
+            using (StreamWriter w = File.AppendText(Directory.GetCurrentDirectory() + @"\log.txt"))
+            {
+                Log("Program Starting - Current Uptime of System is: " + getUpTime() + " hours", w);
+            }
+
+           
+            bool moreThen24 = false;
+
+
+           
+            while (moreThen24 == false)
+            {
+               
+                int upTime = getUpTime();
+                if (upTime <= 23)
+                {
+                    moreThen24 = false;
+                    using (StreamWriter w = File.AppendText(Directory.GetCurrentDirectory() + @"\log.txt"))
+                    {
+                        Log("Current UpTime:  " + getUpTime() + " hours. The system will check again in a hour.", w);
+                    }
+                    
+                    Thread.Sleep(1000 * 60 * 60); //sleeps for an hour
+                    
+                }
+                else
+                {                    
+                    moreThen24 = true;
+                    using (StreamWriter w = File.AppendText(Directory.GetCurrentDirectory() + @"\log.txt"))
+                    {
+                        Log("Current UpTime is met: Program is launching now" , w);
+                    }
+                    InitializeComponent();
+                    timer1.Start();
+                    internalTimer.Stop();
+
+                }
+
+            }
+           
+        }
+        
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            //cancelhits += 1;
+            //ConfigurationManager.AppSettings["CancelButtonHits"] = (int.Parse(ConfigurationManager.AppSettings["CancelButtonHits"]) + 1).ToString();
+            Hide();
+            timer1.Stop();
+            internalTimer.Start();
+            using (StreamWriter w = File.AppendText(Directory.GetCurrentDirectory() + @"\log.txt"))
+            {
+                Log("User Delayed the Reboot - System has been up for: " + getUpTime() + " hours", w);
+            }
+        }
+
+        private void rebootBtn_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter w = File.AppendText(Directory.GetCurrentDirectory() + @"\log.txt"))
+            {
+                Log("System Rebooted", w);
+            }
+            Process.Start("shutdown", "/r /t 0"); //The argument r is to restart the computer 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+                span = span.Subtract(TimeSpan.FromSeconds(1));
+                timerLabel.Text = span.ToString(@"hh\:mm\:ss");
+            
+            if (span.ToString(@"hh\:mm\:ss") == "00:00:00")
+            {
+                using (StreamWriter w = File.AppendText(Directory.GetCurrentDirectory() + @"\log.txt"))
+                {
+                    Log("System Rebooted", w);
+                }
+                timer1.Stop();                
+                Process.Start("shutdown", "/r /t 0"); //The argument r is to restart the computer 
+            }
+
+        }
+
+        private void internalTimer_Tick(object sender, EventArgs e)
+        {
+ 
+                reminder = reminder.Subtract(TimeSpan.FromSeconds(1));
+            
+
+            if (reminder.ToString(@"hh\:mm\:ss") == "00:00:00")
+            {
+                mainForm restart = new mainForm();
+                restart.Show();
+            }
+
+        }
+
+        
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+ 
+            double times = (RemHour() * 60) + RemMin();
+            cancelBtn.Text = "Delay for " + times.ToString() + " Mins";
+
+            string fileName = (Directory.GetCurrentDirectory() + @"\log.txt");
+            FileInfo fi = new FileInfo(fileName);
+
+            if (File.Exists(fileName))
+            {
+                if (fi.Length > 10240)       // ## NOTE: 10KB max file size
+                {
+                    var lines = File.ReadAllLines((Directory.GetCurrentDirectory() + @"\log.txt")).Skip(10).ToArray();  // ## Set to 10 lines
+                    File.WriteAllLines((Directory.GetCurrentDirectory() + @"\log.txt"), lines);
+                }
+            }
+        }
+        public static void Log(string logMessage, TextWriter w)
+        {
+            w.Write("Log Entry ");
+            w.Write($": {logMessage} ");
+            w.Write($"at {DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}\r\n");
+
+        }
+
+    }
+}
